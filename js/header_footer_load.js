cb29b7663e1a0414e069b9818ed17e2be20c113b
Felipe Grisales Cardona <felipe-grisales@hotmail.com>
2022-07-05 20:10:53 -0500

    Unified Header and footer for all pages

---
+++ js/header_footer_load.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#header-boxa-container").load("/html/common/header-boxa.html");
+  $("#footer-boxa-container").load("/html/common/footer-boxa.html");
+  var chargenow = setTimeout(addActiveClass, 50);
+});
+function addActiveClass() {
+  var itemselected;
+  if (document.title == "How to buy - BOXA") {
+    itemselected = document.querySelector("ul.navbar-nav>li:nth-child(1)");
+  } else if (document.title == "Tokenomics - BOXA") {
+    itemselected = document.querySelector("ul.navbar-nav>li:nth-child(2)");
+  } else if (document.title == "Manifest - BOXA") {
+    itemselected = document.querySelector("ul.navbar-nav>li:nth-child(3)");
+  } else if (document.title == "Real Estate - BOXA") {
+    itemselected = document.querySelector("ul.navbar-nav>li:nth-child(4)");
+  }
+  itemselected.classList.add("active");
+}
