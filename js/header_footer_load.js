d6b7353ad1185f6c485bc1bce3460738a6f2edb6
Felipe Grisales Cardona <felipe-grisales@hotmail.com>
2022-07-25 23:05:14 -0500

    Menu Highligthed Adjusted to new names

--- js/header_footer_load.js
+++ js/header_footer_load.js
@@ -10,9 +10,9 @@ function addActiveClass() {
     itemselected = document.querySelector("ul.navbar-nav>li:nth-child(1)");
   } else if (document.title == "Tokenomics - BOXA") {
     itemselected = document.querySelector("ul.navbar-nav>li:nth-child(2)");
-  } else if (document.title == "Manifest - BOXA") {
+  } else if (document.title == "Manifesto - BOXA") {
     itemselected = document.querySelector("ul.navbar-nav>li:nth-child(3)");
-  } else if (document.title == "Real Estate - BOXA") {
+  } else if (document.title == "Benefits - BOXA") {
     itemselected = document.querySelector("ul.navbar-nav>li:nth-child(4)");
   }
   itemselected.classList.add("active");
